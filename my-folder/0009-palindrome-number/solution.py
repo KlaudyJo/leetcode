f7ff9ccee064a4c0e12155d75a180ebf1a668c60
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2022-05-10 15:04:51 +0000

    [LeetCode Sync] Runtime - 97 ms (5.36%), Memory - 13.2 MB (7.66%)

---
+++ my-folder/0009-palindrome-number/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0 or (x % 10 == 0 & x!=0):
+            return False
+        number =x
+        reverse = 0
+        while number:
+            reverse = reverse*10 + number%10
+            number //=10
+        return x == reverse
+        
