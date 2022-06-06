81029457cf19cf63fe4cd7827073638e0a5762e6
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2022-06-06 14:49:18 +0000

    [LeetCode Sync] Runtime - 26 ms (6.73%), Memory - 13.3 MB (19.37%)

---
+++ my-folder/0782-jewels-and-stones/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        jewel =set(jewels)
+        return sum(s in jewel for s in stones)
