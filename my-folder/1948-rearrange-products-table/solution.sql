e005bf8ba780756902496ba9312a4c2c189acd41
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2022-06-07 19:53:50 +0000

    [LeetCode Sync] Runtime - 725 ms (87.09%), Memory - 0B (100.00%)

--- my-folder/1948-rearrange-products-table/solution.sql
+++ my-folder/1948-rearrange-products-table/solution.sql
@@ -1,14 +1,9 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 
-
-select product_id, 'store1' store, store1 price
+SELECT product_id, store, price
 FROM Products
-where store1 IS NOT NULL
-UNION
-select product_id, 'store2' store, store2 price
-FROM Products
-where store2 IS NOT NULL
-UNION
-select product_id, 'store3' store, store3 price
-FROM Products
-where store3 IS NOT NULL;
+UNPIVOT
+(
+price
+for store in (store1, store2, store3))
+as StorePivot;
