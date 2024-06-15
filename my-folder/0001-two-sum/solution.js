e30123d67da79c3a7e2b093e1f4af0c2d4f003fe
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2024-06-15 18:32:46 +0000

    [LeetCode Sync] Runtime - 56 ms (80.48%), Memory - 51.3 MB (28.10%)

---
+++ my-folder/0001-two-sum/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hashMap = {}
+    for (let i=0; i < nums.length; i++){
+        const diff = target - nums[i]
+        if ( diff in hashMap) {
+            return [hashMap[diff], i]
+        }
+        hashMap[nums[i]]= i
+    }
+};
