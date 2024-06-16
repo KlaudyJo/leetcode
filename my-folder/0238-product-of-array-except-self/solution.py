bf2042d307a6c475479b78b3b0fa4e12dc7d431b
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2024-06-16 12:44:02 +0000

    [LeetCode Sync] Runtime - 274 ms (43.90%), Memory - 25.5 MB (96.48%)

--- my-folder/0238-product-of-array-except-self/solution.py
+++ my-folder/0238-product-of-array-except-self/solution.py
@@ -1,23 +1,18 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        # sol = [1]*(len(nums))
-        # pre = 1
-        # for i in range(len(nums)):
-        #     sol[i] = pre
-        #     pre *= nums[i]
-        # post = 1
-        # for i in range(len(nums) -1,-1,-1):
-        #     sol[i] *= post
-        #     post *= nums[i]
-        # return sol
-        p = [1] * len(nums); backward = 1
-        for i in range(1, len(nums)):
-            p[i] = p[i-1] * nums[i - 1]
-        for i in range(len(nums)-1, -1, -1):
-            p[i] *= backward
-            backward *= nums[i]
-        return p
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        array_length = len(nums)
+        result = [1] * array_length
+
+        left_product = 1
+        right_product = 1
+
+        for i in range(array_length):
+            result[i] = left_product
+            left_product *= nums[i]
+
+        for i in range(array_length - 1, -1, -1):
+            result[i] *= right_product
+            right_product *= nums[i]
+        
+        return result
+
