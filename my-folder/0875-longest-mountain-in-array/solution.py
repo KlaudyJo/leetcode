0b328a721484f97a9f912be9b5bce182801c0105
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2025-08-24 19:15:54 +0000

    [LeetCode Sync] Runtime - 8 ms (86.92%), Memory - 18.9 MB (35.31%)

---
+++ my-folder/0875-longest-mountain-in-array/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestMountain(self, arr: List[int]) -> int:
+        res = 0
+        array_len = len(arr)-1
+        for i in range(1, array_len):
+            # check for peak
+            if arr[i-1] < arr[i] > arr[i+1]:
+                l=r=i
+                while l > 0 and arr[l] > arr[l-1]:
+                    l-= 1
+
+                while r < array_len and arr[r] > arr[r+1]:
+                    r+= 1
+
+                res = max(res, r-l+1)
+        return res
+
+                
+        
