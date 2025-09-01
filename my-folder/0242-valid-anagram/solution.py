d395f0d4db3008ff8aba4404747494d5f5b0d0f1
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2025-09-01 18:09:16 +0000

    [LeetCode Sync] Runtime - 17 ms (74.39%), Memory - 14.5 MB (10.10%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,19 +1,25 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
         smap = {}
         sl = len(s)
         tl = len(t)
-        
+
         if sl != tl:
             return False
-        
+
         for i in range(sl):
             smap[s[i]] = smap.get(s[i], 0) + 1
             smap[t[i]] = smap.get(t[i], 0) - 1
         
-        for count in smap.values():
-            if count != 0:
+        for c in smap.values():
+            if c != 0:
                 return False
         
         return True
-        
+
+
