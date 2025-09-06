fd766bda15ecf4554a778ec4d45ab70ebfd288e5
KlaudyJo <87180816+KlaudyJo@users.noreply.github.com>
2025-09-06 19:31:57 +0000

    [LeetCode Sync] Runtime - 16 ms (84.26%), Memory - 13.3 MB (15.75%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -4,14 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        charset = set()
-        
-        l =0
+
         res = 0
-        for r in range(len(s)):
-            while s[r] in charset:
-                charset.remove(s[l])
-                l +=1
-            charset.add(s[r])
-            res =max(res, r-l + 1)
+        char = set()
+        l = 0
+        #"abcabcbb"
+
+        for r in range((len(s))):
+            while s[r] in char:
+                char.remove(s[l])
+                l += 1
+            char.add(s[r])
+            res = max(res, r-l + 1)
+
         return res
+
+
+        
